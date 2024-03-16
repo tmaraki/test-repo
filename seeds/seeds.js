e545a5819ae03a60ef85ef57b853a214a02bf7d3
tmaraki <150744204+tmaraki@users.noreply.github.com>
2024-03-16 17:51:48 -0400

    updated seeds.js file

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,9 +1,8 @@
 const sequelize = require('../config/connection');
-const { User, Location, Review } = require('../models');
+const { User, Location } = require('../models');
 
 const userData = require('./userData.json');
 const locationData = require('./locationData.json');
-const reviewData = require('./reviewData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
